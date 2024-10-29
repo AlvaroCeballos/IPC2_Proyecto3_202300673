876e8a264e759d3753d470ec1e69ed842a164a4e
AlvaroCeballos <a.ceballos.ivddb@gmail.com>
2024-10-28 22:07:44 -0600

    Medio respuesta

--- Frontend/proyecto3/urls.py
+++ Frontend/proyecto3/urls.py
@@ -5,4 +5,7 @@ from . import views
 urlpatterns = [ 
     path('', views.index, name='index'),
     path('configurar/', views.configurar, name='configurar'),
+    path('visualizarXML/', views.visualizarXML, name='visualizarXML'),
+    path('subirXML/', views.subirXML, name='subirXML'),
+    path('generarResultados/', views.generarResultados, name='generarResultados'),
 ]
--- Frontend/proyecto3/views.py
+++ Frontend/proyecto3/views.py
@@ -6,6 +6,24 @@ def index(request):
     return render(request, 'index.html')
 
 def configurar(request):
-    return render(request, 'configurar.html')
+    return render(request, 'CargarArchivo.html')
+
+def visualizarXML(request):
+    if request.method == 'POST':
+        xml_file = request.FILES['file']
+        xml_content = xml_file.read().decode('utf-8')
+        return render(request, 'CargarArchivo.html', {'xml_content': xml_content})
+    return render(request, 'CargarArchivo.html')
+
+def subirXML(request):
+    if request.method == 'POST':
+        xml_content = request.POST['xml']
+        response = requests.post('http://localhost:5000/config/postDiccionarioXML', data=xml_content, headers={'Content-Type': 'text/xml'})
+        return render(request, 'CargarArchivo.html', {'response': response.text})
+    return render(request, 'CargarArchivo.html')
+
+def generarResultados(request):
+    response = requests.get('http://localhost:5000/config/obtenerDiccionario')
+    return render(request, 'CargarArchivo.html', {'response': response.text})
 
 # Create your views here.
