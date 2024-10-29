fc0e8e8d93dcbfbec0cea10b80a0599cb6013fa8
AlvaroCeballos <a.ceballos.ivddb@gmail.com>
2024-10-28 22:19:28 -0600

    mENSAJE PRUEBA

--- Frontend/proyecto3/urls.py
+++ Frontend/proyecto3/urls.py
@@ -8,4 +8,10 @@ urlpatterns = [
     path('visualizarXML/', views.visualizarXML, name='visualizarXML'),
     path('subirXML/', views.subirXML, name='subirXML'),
     path('generarResultados/', views.generarResultados, name='generarResultados'),
+
+
+    path('configurar2/', views.configurar2, name='configurar2'),
+    path('visualizarXML2/', views.visualizarXML2, name='visualizarXML2'),
+    path('subirXML2/', views.subirXML2, name='subirXML2'),
+    path('generarResultados2/', views.generarResultados2, name='generarResultados2'),
 ]
--- Frontend/proyecto3/views.py
+++ Frontend/proyecto3/views.py
@@ -26,4 +26,25 @@ def generarResultados(request):
     response = requests.get('http://localhost:5000/config/obtenerDiccionario')
     return render(request, 'CargarArchivo.html', {'response': response.text})
 
-# Create your views here.
+# -----------------------------------------------------------------------------------------------------------
+
+def configurar2(request):
+    return render(request, 'mensajePrueba.html')
+
+def visualizarXML2(request):
+    if request.method == 'POST':
+        xml_file = request.FILES['file']
+        xml_content = xml_file.read().decode('utf-8')
+        return render(request, 'mensajePrueba.html', {'xml_content': xml_content})
+    return render(request, 'mensajePrueba.html')
+
+def subirXML2(request):
+    if request.method == 'POST':
+        xml_content = request.POST['xml']
+        response = requests.post('http://localhost:5000/config/postMensajePrueba', data=xml_content, headers={'Content-Type': 'text/xml'})
+        return render(request, 'mensajePrueba.html', {'response': response.text})
+    return render(request, 'mensajePrueba.html')
+
+def generarResultados2(request):
+    response = requests.get('http://localhost:5000/config/obtenerDiccionario')
+    return render(request, 'mensajePrueba.html', {'response': response.text})
